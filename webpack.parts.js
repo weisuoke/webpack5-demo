11bbea09ab9a6a1f450c51d2e94417b7c2dc5462
梅林的法杖 <530264127@qq.com>
2021-01-10 22:44:46 +0800

    css loader

---
+++ webpack.parts.js
@@ -0,0 +1,28 @@
+const { WebpackPluginServe } = require("webpack-plugin-serve");
+const {
+  MiniHtmlWebpackPlugin
+} = require("mini-html-webpack-plugin")
+
+exports.devServer = () => ({
+  watch: true,
+  plugins: [
+    new WebpackPluginServe({
+      port: process.env.PORT || 8080,
+      static: './dist', // Expose if output.path changes
+      liveReload: true,
+      waitForBuild: true
+    })
+  ]
+})
+
+exports.page = ({ title }) => ({
+  plugins: [new MiniHtmlWebpackPlugin({ context: { title } })],
+})
+
+exports.loadCSS = () => ({
+  module: {
+    rules: [
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+    ],
+  },
+});
